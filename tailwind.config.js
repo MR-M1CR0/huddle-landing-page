934b3b910bfaeae8795869e751c22cf0deeebbf7
Ahmed Mahfouz <ahmedmahfouz098@gmail.com>
2022-08-19 13:50:49 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primaryViolet: 'hsl(257, 40%, 49%)',
+        softMagenta: 'hsl(300, 69%, 71%)',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        opensans: ['Open Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
